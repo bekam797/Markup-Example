b3a8173791f7f0f469ff485e8f71927b2357b05b
Beka <bekamakharoblishvili@gmail.com>
2020-09-16 17:37:39 +0400

    Add icons

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Logo from '../../assets/svg/logo.svg';
 import Coin from '../../assets/svg/coin.svg';
 import Settings from '../../assets/svg/settings.svg';
+import OnlineUser from '../../assets/svg/onlineUser.svg';
+import OfflineUser from '../../assets/svg/offlineUser.svg';
+import Cherrie from '../../assets/svg/cherrie.svg';
+import Time from '../../assets/svg/time.svg';
+import GoldCoin from '../../assets/svg/goldcoin.svg';
+import Spin from '../../assets/svg/spin.svg';
 
 const Icons = (props) => (
   <img
@@ -12,8 +18,21 @@ const Icons = (props) => (
         ? Coin
         : props.iconType === 'settings'
         ? Settings
+        : props.iconType === 'online'
+        ? OnlineUser
+        : props.iconType === 'offline'
+        ? OfflineUser
+        : props.iconType === 'cherrie'
+        ? Cherrie
+        : props.iconType === 'time'
+        ? Time
+        : props.iconType === 'goldcoin'
+        ? GoldCoin
+        : props.iconType === 'spin'
+        ? Spin
         : null
     }
+    className={props.iconStyle === 'online' ? 'icon' : ''}
     alt="svg"
   />
 );
